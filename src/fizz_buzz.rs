362b7b23c2f1a2dc9ba2866bd6bbf67250d18e28
t-jimbo <jbear.um@gmail.com>
2023-12-09 23:50:28 +0900

    feat: implement fizz buzz

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -1,3 +1,17 @@
-pub fn fizz_buzz() {
-    println!("test fizz buzz");
+pub fn fizz_buzz(num: i32) {
+    for i in 1..=num {
+        if i % 15 == 0 {
+            println!("fizz buzz");
+            continue;
+        }
+        if i % 3 == 0 {
+            println!("fizz");
+            continue;
+        }
+        if i % 5 == 0 {
+            println!("buzz");
+            continue;
+        }
+        println!("{}", i);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,5 @@
 mod fizz_buzz;
 
 fn main() {
-    hello("taro");
-
-    fizz_buzz::fizz_buzz();
-}
-
-// print hello
-fn hello(name: &str) {
-    println!("Hello, {}", name)
+    fizz_buzz::fizz_buzz(15);
 }
